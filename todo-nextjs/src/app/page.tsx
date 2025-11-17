9a0c42a44871136d05eacae656474ddd3f0f3660
udud0099 <udud0099@gmail.com>
2025-11-17 16:32:23 +0545

    READ - get data

--- todo-nextjs/src/app/page.tsx
+++ todo-nextjs/src/app/page.tsx
@@ -1,14 +1,36 @@
 
-import Form from "../../components/form/Form";
+ import { prisma } from "@/utils/prisma";
 import AddTodo from "../../components/todos/AddTodo";
 
-export default function Home() {
+
+ async function getData(){
+  const data = await prisma.todo.findMany({
+    select: {
+      title: true,
+      id: true,
+      isCompleted:true,
+    },
+    orderBy:{
+      createdAt:"asc"
+    }
+  })
+  return data
+ }
+
+export default async function Home() {
+  const data = await getData()
   return (
      <>
      <div>
       <h1>home page</h1>
       <div>
         <AddTodo />
+      </div>
+      <br />
+      <div>
+        {data.map((todo, id)=>(
+          <div   key={id} className="my-1">{todo.title}</div>
+        ))}
       </div>
      </div>
      </>
