daf8e49523e85fb978036a276da57d8246c0222a
udud0099 <udud0099@gmail.com>
2025-12-01 22:21:46 +0545

    user model create

--- restapi/app/api/users/route.ts
+++ restapi/app/api/users/route.ts
@@ -1,6 +1,16 @@
+import connect from "@/lib/db"
+import User from "@/lib/modals/user"
 import { NextResponse } from "next/server"
 
 
-export const GET = () =>{
-    return new NextResponse("this is my first api")
+export const GET = async () =>{
+    try {
+        await connect()
+        const users = await User.find()
+        return new NextResponse(JSON.stringify(users), {status:200})
+        
+    } catch (error:any) {
+        
+        return new NextResponse("error in get data" + error.message, {status:500})
+    }
 }
