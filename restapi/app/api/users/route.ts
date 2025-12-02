0657dd5ebfa50c986405b783d9a880c0a0a485b6
udud0099 <udud0099@gmail.com>
2025-12-02 09:51:52 +0545

    rest api POST api create

--- restapi/app/api/users/route.ts
+++ restapi/app/api/users/route.ts
@@ -14,3 +14,21 @@ export const GET = async () =>{
         return new NextResponse("error in get data" + error.message, {status:500})
     }
 }
+
+export const POST = async (request: Request) =>{
+    try {
+        const body = await request.json()
+        await connect()
+        const newUser = new User(body)
+        await newUser.save()
+        return new NextResponse(JSON.stringify(
+            {
+                message:"user is created", user: newUser 
+            }
+        ), {status:200})
+        
+    } catch (error:any) {
+        
+        return new NextResponse("error in get data" + error.message, {status:500})
+    }
+}
